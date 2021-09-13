721468c3f3091df55ebde3212f44d1e9346d0bb3
Daniel Lima <73969685+bolodissenoura@users.noreply.github.com>
2021-09-13 09:44:47 -0300

    Initial Commit

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+  <web-view
+    class="cont"
+    :source="{
+      uri: 'https://crossdocking.homologacao.boostech.com.br/mobile/',
+    }"
+  />
+</template>
+
+<script>
+import { WebView } from "react-native-webview";
+
+export default {
+  name: "myComponent",
+  components: {
+    "web-view": WebView,
+  },
+};
+</script>
+<style>
+</style>
+
---
+++ pages/inicio.vue
@@ -0,0 +1,27 @@
+<template>
+  <web-view
+    :source="{ uri: 'https://crossdocking.homologacao.boostech.com.br/' }"
+  />
+</template>
+
+<script>
+import { WebView } from "react-native-webview";
+
+export default {
+  name: "myComponent",
+  components: {
+    "web-view": WebView,
+  },
+};
+</script>
+<style>
+.container {
+  background-color: pink;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: black;
+}
+</style>
